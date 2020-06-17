a948840eb1473dae4d9c433051e46fba34dedcbe
AnmolKumar Shah <anmolshah.20k@gmail.com>
2020-06-17 16:33:12 +0530

    final

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ import thunk from 'redux-thunk'
 import {getFirebase, ReactReduxFirebaseProvider} from 'react-redux-firebase'
 import firebase from './Config/firebaseConfig'
 import {createFirestoreInstance} from 'redux-firestore'
+import {useSelector} from 'react-redux'
+import {isLoaded} from 'react-redux-firebase'
 
 
 const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({getFirebase})))
@@ -24,11 +26,29 @@ const rrfProps = {
   createFirestoreInstance
 }
 
+function AuthIsLoaded({children}){
+  const auth = useSelector(state => state.firebase.auth);
+  if(!isLoaded(auth))
+  return(
+    <div className='text-center'>
+      <div className='spinner-border  text-primary'
+      style={{width : '10rem', height: '10rem', marginTop:'100px' }}
+      role="status"
+      >
+        <span className='sr-only'>Loading. . .</span>
+      </div>
+    </div>
+  );
+  return children;
+}
+ 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ReactReduxFirebaseProvider {...rrfProps}>
-        <App />
+        <AuthIsLoaded>
+          <App />
+        </AuthIsLoaded>
       </ReactReduxFirebaseProvider>
     </Provider>
   </React.StrictMode>,
