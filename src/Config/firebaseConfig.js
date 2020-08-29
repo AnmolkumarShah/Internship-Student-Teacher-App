f1c450eece8e162deaa48accf65201a54b328831
Anmol Shah <52107296+AnmolkumarShah@users.noreply.github.com>
2020-08-29 14:42:34 +0530

    key removed

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -3,13 +3,13 @@ import "firebase/firestore"
 import 'firebase/auth'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAtPJ9-wWhu2WJHGm87tClv2VGzoTT3WXg",
+  apiKey: "##################################",
   authDomain: "student-teacher-60724.firebaseapp.com",
   databaseURL: "https://student-teacher-60724.firebaseio.com",
   projectId: "student-teacher-60724",
   storageBucket: "student-teacher-60724.appspot.com",
-  messagingSenderId: "131601404057",
-  appId: "1:131601404057:web:033a223b1094f678eee808"
+  messagingSenderId: "####################",
+  appId: "#########################################"
 };
 
 firebase.initializeApp(firebaseConfig);
